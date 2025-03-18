479d389edf97c4c08bd16b45ef8094947841d3f7
Greg Martinez <72076948+greg-martinez44@users.noreply.github.com>
2025-03-17 17:36:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,10 +7,9 @@ from src import models as m
 URL = "https://www.mtgtop8.com/format?f=ST"
 
 def main():
-    updater = m.Updater()
-    updater.update_events(URL)
-    updater.update_decks_and_players()
-    updater.update_deck_lists()
+    m.update_events(URL)
+    m.update_decks_and_players()
+    m.update_deck_lists()
 
 if __name__ == "__main__":
     main()
